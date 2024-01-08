e2306da571597366159ff5e5cf203ab02c42e4c0
cocktailpeanut <cocktailpeanuts@proton.me>
2024-01-07 23:07:39 -0500

    space

--- demo/realtime-img2img/main.py
+++ demo/realtime-img2img/main.py
@@ -163,7 +163,7 @@ if torch.cuda.is_available():
 elif torch.backends.mps.is_available():
     device = "mps"
 else:
-     device = "cpu"
+    device = "cpu"
 #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch_dtype = torch.float16
 pipeline = Pipeline(config, device, torch_dtype)
